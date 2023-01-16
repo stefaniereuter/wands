204784d2758133ded23d091e4a4b4a4416a1230c
Toby James <toby.james@ukaea.uk>
2023-01-16 09:29:29 +0000

    Setup to interface with HDF5

--- python/dataman-receiver.py
+++ python/dataman-receiver.py
@@ -1,22 +1,30 @@
 import numpy as np
 import adios2
-#import socket
+
+# import socket
 
 Nx = 4
 Ny = 4
 adios = adios2.ADIOS()
 
 datamanIO = adios.DeclareIO("Fluesterpost")
-#specify engine Dataman receive Data from dataman server program BP5 read data that was previously written.
+# specify engine Dataman receive Data from dataman server program BP5 read data that was previously written.
 datamanIO.SetEngine("Dataman")
-#datamanIO.SetEngine("BP5")
+# datamanIO.SetEngine("BP5")
 
-#specify parameters for dataman connection 
-#specify the IP of the host that is receiving the data. Either specified prior or uncomment import socket and inquire IP via gethostbyaddr. 
-#datamanIO.SetParameters({"IPAddress":"10.43.78.89" , "Port":"12306", "Timeout":"5", "TransportMode":"reliable" })
-#IP 127.0.0.1 if on same host as server code
-datamanIO.SetParameters({"IPAddress":"127.0.0.1" , "Port":"12306", "Timeout":"5", "TransportMode":"reliable" })
-#datamanIO.SetParameters({"IPAddress":socket.gethostbyaddr("cpu-q-526.data.cluster")[2][0]  , "Port":"12306", "Timeout":"5", "TransportMode":"reliable" })
+# specify parameters for dataman connection
+# specify the IP of the host that is receiving the data. Either specified prior or uncomment import socket and inquire IP via gethostbyaddr.
+# datamanIO.SetParameters({"IPAddress":"10.43.78.89" , "Port":"12306", "Timeout":"5", "TransportMode":"reliable" })
+# IP 127.0.0.1 if on same host as server code
+datamanIO.SetParameters(
+    {
+        "IPAddress": "127.0.0.1",
+        "Port": "12306",
+        "Timeout": "5",
+        "TransportMode": "reliable",
+    }
+)
+# datamanIO.SetParameters({"IPAddress":socket.gethostbyaddr("cpu-q-526.data.cluster")[2][0]  , "Port":"12306", "Timeout":"5", "TransportMode":"reliable" })
 
 
 # datamanRec = datamanIO.Open("Fluesterpost_matrix1",adios2.Mode.Read)
@@ -30,26 +38,25 @@ datamanIO.SetParameters({"IPAddress":"127.0.0.1" , "Port":"12306", "Timeout":"5"
 #     receivebuffer1 = np.zeros(Nx*Ny)
 #     datamanRec.Get(data1, receivebuffer1, adios2.Mode.Sync)
 #     datamanRec.Close()
-# else: 
+# else:
 #     print("Nullpointer data1")
-
 
 
 # Send all data in one step: Size is known beforehand
 
-#preallocate data variable is possible here if size is constant over every step and if size is known 
-receivedMatrix = np.zeros(Nx*Ny) 
+# preallocate data variable is possible here if size is constant over every step and if size is known
+receivedMatrix = np.zeros(Nx * Ny)
 # InquireVariable needs to happen within the step environment as it will return a nullpointer otherwise.
-#todo: the return of nullpointer outside of a step environment might be a bug and needs to be investigated. 
+# todo: the return of nullpointer outside of a step environment might be a bug and needs to be investigated.
 
-datamanReader = datamanIO.Open("Fluesterpost",adios2.Mode.Read)
+datamanReader = datamanIO.Open("Fluesterpost", adios2.Mode.Read)
 while True:
     stepStatus = datamanReader.BeginStep()
     if stepStatus == adios2.StepStatus.OK:
         data1 = datamanIO.InquireVariable("matrix1")
         if data1:
-            datamanReader.Get(data1,receivedMatrix, adios2.Mode.Sync)
-            currentStep =datamanReader.CurrentStep()
+            datamanReader.Get(data1, receivedMatrix, adios2.Mode.Sync)
+            currentStep = datamanReader.CurrentStep()
             datamanReader.EndStep()
             print("Step", currentStep, receivedMatrix)
         else:
@@ -61,39 +68,33 @@ datamanReader.Close()
 
 print("------------------")
 print(receivedMatrix)
-print("maximum entry: ",receivedMatrix.max())
+print("maximum entry: ", receivedMatrix.max())
 print("data mean: ", receivedMatrix.mean())
 print("minimum entry: ", receivedMatrix.min())
 
 
-#Data is sent in multiple steps. Each step has a different matrix size Currently only the last step will be saved in received matrix. 
-datamanReader = datamanIO.Open("Fluesterpost_matrix",adios2.Mode.Read)
+# Data is sent in multiple steps. Each step has a different matrix size Currently only the last step will be saved in received matrix.
+datamanReader = datamanIO.Open("Fluesterpost_matrix", adios2.Mode.Read)
 while True:
     stepStatus = datamanReader.BeginStep()
     if stepStatus == adios2.StepStatus.OK:
-        #inquire for matrix 2
+        # inquire for matrix 2
         data2 = datamanIO.InquireVariable("matrix2")
         if data2:
-            #determin the shape of the data that will be sent 
+            # determin the shape of the data that will be sent
             buffersize = data2.Shape()
-            #allocate the buffer 
+            # allocate the buffer
             receivedMatrix2 = np.zeros(buffersize)
-            #print(np.prod(buffersize))
-            #todo inquire size via metadata
-            datamanReader.Get(data2,receivedMatrix2, adios2.Mode.Sync)
-            currentStep =datamanReader.CurrentStep()
+            # print(np.prod(buffersize))
+            # todo inquire size via metadata
+            datamanReader.Get(data2, receivedMatrix2, adios2.Mode.Sync)
+            currentStep = datamanReader.CurrentStep()
             datamanReader.EndStep()
         else:
             print("Nullpointer check failed for matrix2")
 
-        
         print("Step", currentStep, receivedMatrix2)
     elif stepStatus == adios2.StepStatus.EndOfStream:
         print("Done Dataset 2")
         break
 datamanReader.Close()
-
-
-
-
-
--- python/dataman-server.py
+++ python/dataman-server.py
@@ -1,44 +1,63 @@
 import sys
-
+import h5py
 import numpy as np
 import adios2
-#import socket
+
+# import socket
 
 # User data
 
-#calculate global number of entries in per direction
+# calculate global number of entries in per direction
 Nx = 4
 Ny = 4
 
 nsteps = Ny
-count = [Nx, Ny]
-start = [0, 0]
-shape = [Nx, Ny]
-data = np.random.rand(Nx,Ny)
+#count = [Nx, Ny]
+#start = [0, 0]
+#shape = [Nx, Ny]
+#data = np.random.rand(Nx, Ny)
+#
+
+axis = "amc"
+h5file = h5py.File("test.h5", 'r')
+data = h5file[axis]
+shape = data.shape
+count = shape
+# use io.BYtes to stream ?
+
 
 print("------------------")
 print(data)
-print("maximum entry: ",data.max())
+print("maximum entry: ", data.max())
 print("data mean: ", data.mean())
 print("minimum entry: ", data.min())
 adios = adios2.ADIOS()
 
 # ADIOS IO
 datamanServer = adios.DeclareIO("Server1")
-#specify the used Engine BP5 writes a file Dataman sends data to waiting receiver code
-#datamanServer.SetEngine('BP5')
-datamanServer.SetEngine('Dataman')
+# specify the used Engine BP5 writes a file Dataman sends data to waiting receiver code
+# datamanServer.SetEngine('BP5')
+datamanServer.SetEngine("Dataman")
 
-#necessary parameters for dataman engine
+# necessary parameters for dataman engine
 # # print(socket.gethostbyaddr("cpu-q-526.data.cluster")[2][0] )
 
 # IP: 0.0.0.0 "listens" to all incoming IPs on specified ports, Timeout is for the put get call after connection is established, Transport mode reliable is that all data is collected (fast would potentially loose some steps)
-datamanServer.SetParameters({"IPAddress":"0.0.0.0" , "Port":"12306", "Timeout":"5", "TransportMode":"reliable" })
+datamanServer.SetParameters(
+    {
+        "IPAddress": "0.0.0.0",
+        "Port": "12306",
+        "Timeout": "5",
+        "TransportMode": "reliable",
+    }
+)
 
 
 # ADIOS Engine
 datamanWriter = datamanServer.Open("data1", adios2.Mode.Write)
-sendbuffer = datamanServer.DefineVariable("matrix1",data,shape,start,count,adios2.ConstantDims)
+sendbuffer = datamanServer.DefineVariable(
+    axis, data, shape, start, count, adios2.ConstantDims
+)
 if sendbuffer:
     datamanWriter.BeginStep()
     datamanWriter.Put(sendbuffer, data)
@@ -56,17 +75,17 @@ datamanWriter.Close()
 # datamanWriter2.Close()
 
 
-#Send data in steps
+# Send data in steps
 datamanWriter3 = datamanServer.Open("Fluesterpost_matrix", adios2.Mode.Write)
-data2 = np.random.rand(1,Nx)
-sendbuffer3 = datamanServer.DefineVariable("matrix2",data2,[Nx,1],[0,0],[Nx,1])
+data2 = np.random.rand(1, Nx)
+sendbuffer3 = datamanServer.DefineVariable("matrix2", data2, [Nx, 1], [0, 0], [Nx, 1])
 if sendbuffer3:
-    for i in range(0,nsteps):
-        stepdatasize = Nx+i
-        data2 = np.random.rand(1,stepdatasize)
-        sendbuffer3.SetShape([stepdatasize,1])
-        sendbuffer3.SetSelection([[0,0],[stepdatasize,1]])
-        #sendbuffer3 = datamanServer.DefineVariable("matrix2",data2,[stepdatasize,1],[0,0],[stepdatasize,1],adios2.ConstantDims)
+    for i in range(0, nsteps):
+        stepdatasize = Nx + i
+        data2 = np.random.rand(1, stepdatasize)
+        sendbuffer3.SetShape([stepdatasize, 1])
+        sendbuffer3.SetSelection([[0, 0], [stepdatasize, 1]])
+        # sendbuffer3 = datamanServer.DefineVariable("matrix2",data2,[stepdatasize,1],[0,0],[stepdatasize,1],adios2.ConstantDims)
         print("step data: ", data2[:])
         datamanWriter3.BeginStep()
         datamanWriter3.Put(sendbuffer3, data2)
